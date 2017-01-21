b514bdef90ef708dbfd59962d6c1cf1fb17b2ca8
Stas <w3oks@mail.ru>
2017-01-21 21:14:52 +0200

    TypesOfData

---
+++ src/com/company/Lesson1TypesOfData.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by Stas on 21.01.2017.
+ */
+public class Lesson1TypesOfData {
+    public static void main(String[] args) {
+
+        int age = 18;
+        String name = "Коле";
+        double height = 1.8;
+
+        System.out.println(name +" уже "+age+" лет, он ростом "+height +" метра.");
+    }
+
+}
