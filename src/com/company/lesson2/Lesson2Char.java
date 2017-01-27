8cdfcafafb07d06084a82f16a0695f479f2f10f0
kvengerov <kvengerov@gmail.com>
2017-01-27 12:35:33 +0200

    upd Lesson2_2

--- src/com/company/lesson2/Lesson2Char.java
+++ src/com/company/lesson2/Lesson2Char.java
@@ -5,10 +5,13 @@ public class Lesson2Char {
 
         char ch = 32;
         char alfA = 'а';
+        char alfAa = 'А';
 
         for (int i = 0; i < ch; i++) {
             System.out.print((char) ('а' + i) + " = ");
-            System.out.println((int) (alfA + i));
+            System.out.print((int) (alfA + i) + " ");
+            System.out.print((char) ('А' + i) + " = ");
+            System.out.println((int) (alfAa + i));
         }
     }
 }
