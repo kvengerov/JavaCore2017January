9d6e37141dae35412472a7cc3ec9a460781138e7
Konstantin <kvengerov@gmail.com>
2017-01-28 20:11:21 +0200

    upd Lesson2_4

--- src/com/company/lesson2/Lesson2Salary.java
+++ src/com/company/lesson2/Lesson2Salary.java
@@ -3,18 +3,18 @@ package com.company.lesson2;
 public class Lesson2Salary {
     public static void main(String[] args) {
 
-        String name = "Виталий заработал 30 рублей, Гена заработал 50 рублей, Дима заработал 60 рублей.";
+        String name = "Виталий заработал 30 рублей, Гена заработал 50 рублей, Дмитрий - 35 рублей.";
 
         System.out.println(name);
 
         int firstMan1 = name.indexOf("заработал");
-        int firstMan2 = name.indexOf("рублей");
+        int firstMan2 = name.indexOf("руб");
         String text1 = name.substring(firstMan1 + 9, firstMan2).trim();
         System.out.println(text1);
 
-        int lastMan1 = name.lastIndexOf("заработал");
-        int lastMan2 = name.lastIndexOf("рублей");
-        String text2 = name.substring(lastMan1 + 9, lastMan2).trim();
+        int lastMan1 = name.lastIndexOf("-");
+        int lastMan2 = name.lastIndexOf("руб");
+        String text2 = name.substring(lastMan1 + 2, lastMan2).trim();
         System.out.println(text2);
 
         System.out.println (Integer.parseInt(text1) + Integer.parseInt(text2));
