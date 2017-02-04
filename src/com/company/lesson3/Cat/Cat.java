d59c706ad2a38f1955faae994a5e2ca4b03b321f
Konstantin <kvengerov@gmail.com>
2017-02-04 16:03:24 +0200

    err Cat

--- src/com/company/lesson3/Cat/Cat.java
+++ src/com/company/lesson3/Cat/Cat.java
@@ -6,64 +6,101 @@ public class Cat {
 
     private Double minWeight;
     private Double maxWeight;
-
     private Double lostWeight;
+
     private Double amountFeed;
     private Double amountDrink;
 
     public Cat() {
-        weight = 1500.0 + 3000.0 * Math.random();
+        //weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
     }
+    public void weight(double kg){
+        weight = kg;
+    }
 
-    public void meow() {
+
+//    public void meow() {
+//        weight--;
+//        System.out.println("Meow");
+//    }
+
+    public void eaten(Double amountEat) {
+        amountFeed = amountEat;
+        weight = weight + amountEat;
+
+    }
+
+    public void play() {
         weight--;
-        System.out.println("Meow");
+        System.out.println("I'm playing! Meow!");
     }
 
-    public void feed(Double amount) {
+//    public void feed(Double amount) {
+//
+//        //передача количества выпитого из локальной переменной amount в классовую переменную amountFeed
+//        amountFeed = amount;
+//        weight = weight + amount;
+//    }
+//
+//    public void drink(Double amount) {
+//
+//        //передача количества выпитого из локальной переменной amount в классовую переменную amountDrink
+//        amountDrink = amount;
+//        weight = weight + amount;
+//    }
 
-        //передача количества выпитого из локальной переменной amount в классовую переменную amountFeed
-        amountFeed = amount;
-        weight = weight + amount;
-    }
+    //метод подсчета количества съеденного и выпитого, для этого были созданны новые переменные
+    // amountFeed и amountDrink
 
-    public void drink(Double amount) {
-
-        //передача количества выпитого из локальной переменной amount в классовую переменную amountDrink
-        amountDrink = amount;
-        weight = weight + amount;
+    public Double totalAmount() {
+//        Double totalAmount = amountFeed + amountDrink;
+        Double totalAmount = amountFeed;
+        return totalAmount;
     }
 
     public Double getWeight() {
         return weight;
     }
 
-    public String getStatus() {
-        if (weight < minWeight) {
-            return "Поздравляем ваш кот стал мумией!";
-        } else if (weight > maxWeight) {
-            return "Ма-ма-а-а, он взорвался!!!";
-        } else if (weight > originWeight) {
-            return "Спит. Z-z-z-z!";
-        } else {
-            return "Играет. Вуху-у-у-у!";
-        }
-    }
+//    public String getStatus() {
+//        if (weight < minWeight) {
+//            return "Поздравляем ваш кот стал мумией!";
+//        } else if (weight > maxWeight) {
+//            return "Ма-ма-а-а, он взорвался!!!";
+//        } else if (weight > originWeight) {
+//            return "Спит. Z-z-z-z!";
+//        } else {
+//            return "Играет. Вуху-у-у-у!";
+//        }
+//    }
 
-    //метод подсчета количества съеденного и выпитого, для этого были созданны новые переменные amountFeed и amountDrink
-    public Double totalAmount() {
-        Double totalAmount = amountFeed + amountDrink;
-        return totalAmount;
-    }
+    //метод rest уменьшает вес кота, когда тот ходит в туалет. создана локальная переменная lostWeight.
+    // "облегчение" высчитывается рандомно.
 
-    //метод rest уменьшает вес кота, когда тот ходит в туалет. создана локальная переменная lostWeight. "облегчение" высчитывается рандомно.
     public String rest() {
         lostWeight = 1000.0 * Math.random();
         weight = weight - lostWeight;
-        String text = String.format("Кот сходил в туалет и стал легче на %.2f и теперь весит %.2f \n", lostWeight, weight);
+        String text = String.format("Кот стал легче на %.2f и теперь весит %.2f \n", lostWeight, weight);
         return text;
+    }
+
+//    так вот сделай метод changeWeight() в которых происходят проверки на мертвоту, ...
+//    по условиям как в статусе, и только тогда все изменения по весу делать в этом методе,
+//    тогда реализуя в этом методе все, ты нигде не забудешь проверки
+
+    public String changeWeight() {
+        if (weight < minWeight) {
+            return "Dead";
+        } else if (weight > maxWeight) {
+            return "Booom";
+        } else if (weight > originWeight) {
+            return "Sleep";
+        } else {
+            play();
+            return "Play";
+        }
     }
 }
--- src/com/company/lesson3/Cat/Loader.java
+++ src/com/company/lesson3/Cat/Loader.java
@@ -1,20 +1,71 @@
 package com.company.lesson3.Cat;
+
+import java.util.Scanner;
+
 /*  Создать в классе Cat метод, который будет возвращать массу съеденной еды.
 *   Создать в классе Cat метод “сходить в туалет”, который будет уменьшать вес кошки и что-нибудь печатать.
 */
 public class Loader {
     public static void main(String[] args) {
         Cat cat = new Cat();
+
+        System.out.print("Enter cat's weight - ");
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble();
+        cat.weight(num);
+
         System.out.printf("Вес кота %.2f \n", cat.getWeight());
-        System.out.println("Кот сейчас: " + cat.getStatus() + "\n");
+        //System.out.println("Кот сейчас: " + cat.changeWeight() + "\n");
 
-        cat.feed(220.0);
-        cat.drink(50.0);
+//        cat.feed(220.0);
+//        cat.drink(50.0);
 
-        System.out.printf("Было съедено и выпито: %.2f теперь вес кота стал: %.2f \n", cat.totalAmount(), cat.getWeight());
+        System.out.println("What are we doing?\n 1. Play\n 2. Eat\n 3. Rest");
+//        Scanner sc1 = new Scanner(System.in);
+        int answer = sc.nextInt();
 
+//        for (int i = 1;;) {
+//            cat.play();
+//            System.out.println("Кот сейчас: " + cat.changeWeight());
+//            for (int j = 2;;) {
+//                double eat = sc.nextDouble();
+//                cat.eaten(eat);
+//                System.out.printf("Было съедено и выпито: %.2f \n", cat.totalAmount());
+//                System.out.println("Кот сейчас: " + cat.changeWeight());
+//                for (int k = 3;;){
+//                    System.out.println(cat.rest());
+//                    System.out.println("Кот сейчас: " + cat.changeWeight());
+//                }
+//            }
+//        }
+        int m = 0;
+        for (int i = 1;;) {
+            for (int j = 2;;) {
+                for (int k = 3;;){
+                    if (m == i) {
+                        cat.play();
+                        System.out.println("Кот сейчас: " + cat.changeWeight());
+                    } else if(m == j){
+                        double eat = sc.nextDouble();
+                        cat.eaten(eat);
+                        System.out.printf("Было съедено и выпито: %.2f \n", cat.totalAmount());
+                        System.out.println("Кот сейчас: " + cat.changeWeight());
+                    }else if(m == k) {
+                        System.out.println(cat.rest());
+                        System.out.println("Кот сейчас: " + cat.changeWeight());
+                    }
+                    else {
+                        System.out.println("Cat dead");
+                    }
+                }
+            }
+        }
+//        System.out.printf("Было съедено и выпито: %.2f теперь вес кота стал: %.2f \n", cat.totalAmount(), cat.getWeight());
 
-        System.out.println(cat.rest());
-        System.out.println("Кот сейчас: " + cat.getStatus());
+//        System.out.println("Кот сейчас: " + cat.changeWeight());
+
+//        for (int i = cat.getWeight(); i < ; i++) {
+//
+//        }
     }
 }
