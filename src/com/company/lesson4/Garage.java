4e79975c37a1ba63cf9823078fa624e97afede3a
Konstantin <kvengerov@gmail.com>
2017-02-11 13:43:29 +0200

    change command exitall

--- src/com/company/lesson4/Garage.java
+++ src/com/company/lesson4/Garage.java
@@ -8,34 +8,42 @@ import java.util.ArrayList;
 import static java.lang.System.exit;
 
 public class Garage {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<String> cars = new ArrayList<>();
-        for (;;){
+        for (; ; ) {
             System.out.println("Введите номер машины: ");
             String numberOfCars = reader.readLine();
             cars.add(numberOfCars);
 
-            if(cars.size() > 4){
+            if (cars.size() > 4) {
                 System.out.println("Гараж заполнен. Нет места для парковки.");
                 System.out.println(cars);
 
                 System.out.println("Введите exit для выезда одной машины из гаража. \nВведите exitall для выезда всех машин из гаража. \nВведите close для закрытия программы");
                 String clearCar = reader.readLine();
                 String clearCarLowerCase = clearCar.toLowerCase();
-                switch (clearCarLowerCase){
+                switch (clearCarLowerCase) {
                     case "exit":
-                        String nameCar = cars.remove(cars.size()-1);
+                        String nameCar = cars.remove(cars.size() - 1);
                         System.out.println("Машина " + nameCar + " выехала.");
                         System.out.println(cars);
                         break;
                     case "exitall":
-                        for (int i = -4; i < cars.size(); i++) {
-                            String nameAllCar = cars.remove(cars.size()-1);
-                            System.out.println("Машина " + nameAllCar + " выехала.");
+//                        for (int i = -4; i < cars.size(); i++) {
+//                            String nameAllCar = cars.remove(cars.size()-1);
+//                            System.out.println("Машина " + nameAllCar + " выехала.");
+//                        }
+                        if (cars.size() > 0) {
+                            for (int i = cars.size() - 1; i >= 0; i--) {
+                                String nameAllCar = cars.remove(i);
+                                System.out.println("Машина " + nameAllCar + " выехала.");
+                            }
+                            System.out.println(cars);
+                            System.out.println("Гараж пуст. Все места свободны.");
+                        } else {
+                            System.out.println("Гараж уже пуст. Все места свободны.");
                         }
-                        System.out.println(cars);
-                        System.out.println("Гараж пуст. Все места свободны.");
                         break;
                     case "close":
                         System.out.println("Завершение программы.");
